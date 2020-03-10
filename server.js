5a5cf1c7ee2fa89f39daf005266a4e53d0308edf
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-03-10 03:56:59 +0100

    Added post route

--- server.js
+++ server.js
@@ -10,5 +10,19 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'we sad boiii'));
 db.once('open', () => console.log('We in with mongoose boii'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send('Hello World!'));
+
+app.use(express.json())
+app.listen(8080)
+
+app.post('/save', (req, res) => {
+    const { word, image } = req.body;
+    console.log(req.body);
+
+    console.log(`Word ${word}, image ${image}`);
+
+    res.json({ body: req.body })
+});
+
+
 app.listen(PORT, () => console.log(`We listening to port ${PORT} boiii!`))
